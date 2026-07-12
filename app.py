2b95688e08ee489c214f1f1bbfb4b183ae3a74bb
faisalll-01 <mohdfaisal42856@gmail.com>
2026-07-13 02:17:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ st.title("🏦 Loan Approval Prediction System")
 
 gender =  st.selectbox("Gender", ["Male","Female"])
 gender = gender_encoder.transform([gender])[0]
-married = st.selectbox("Married", ["Yes", "No"])
-married = 0 if married== "No" else 1
+married = st.selectbox("Married", [1, 0])
 dependents = st.selectbox("Dependents", [0, 1, 2, 3])
 education = st.selectbox("Education", [0, 1])
 self_employed = st.selectbox("Self Employed", [0, 1])
